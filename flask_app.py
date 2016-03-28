901079ba46f3f2506c5a82825b8370b38a40f313
ccpics42 <ccpics42@gmail.com>
2016-03-28 05:49:35 +0000

    Fix bug

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-import json 
+import json
 from os import listdir
 from os.path import isfile, join
 
@@ -22,40 +22,39 @@ def register():
 def registersubmit():
 	user_dir = "/home/ccpics42/mysite/pics/data/user/"
 	user_files = [f for f in listdir(user_dir) if isfile(join(user_dir, f))]
-	error = False 
+	error = False
 
 
 	# Get the largest user ID
-	max_id = 0 
+	max_id = 0
 	for user_file in user_files:
 		user_id = int(user_file[:8])
 
-		if user_id > max_id: 
-			max_id = user_id 
+		if user_id > max_id:
+			max_id = user_id
 
 	# Read the files to check if the user exists.
-	# Use email address for uniqueness. 
+	# Use email address for uniqueness.
 	for user_file in user_files:
 		with open(user_dir + user_file, 'r') as f:
 			data = json.load(f)
 
 			if request.form["email"] == data["email"]:
-				error = True 
-			else: 
-				new_file_path = user_dir + ("{0:08d}".format(max_id + 1)) + \
-					".txt"
-				
+				error = True
+			else:
+				new_file_path = user_dir + "{0:08d}".format(max_id + 1) + ".txt"
+
 				with open(new_file_path, 'w') as write_file:
 					user_data = {}
-					user_data["username"] = request.form["username"]		
+					user_data["username"] = request.form["username"]
 					user_data["password"] = request.form["password"]
 					user_data["email"] = request.form["email"]
 					json.dump(user_data, write_file)
 
-		if error: 
-			break 
+		if error:
+			break
 
-	if error: 
+	if error:
 		return "Error: User already exists"
-	else: 
+	else:
 		return "Success! You have been registered!"
