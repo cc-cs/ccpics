472a6028db2f624dabeedab49182519a9ee6e00f
rfrapp <rfrapp@gmail.com>
2016-03-28 13:56:45 -0500

    Add layout file. Make logging in/out functional

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, redirect, request, render_template, url_for, session 
 import json
 from os import listdir
 from os.path import isfile, join
@@ -12,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return '<h1>Welcome to the PiCS website!</h1>'
+    return render_template('index.html')
 
 @app.route('/register')
 def register():
@@ -60,24 +58,49 @@ def registersubmit():
         return "Success! You have been registered!"
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/loginsubmit', methods=['POST'])
 def loginsubmit():
     user_dir = "/home/ccpics42/mysite/pics/data/user/"
     user_files = [f for f in listdir(user_dir) if isfile(join(user_dir, f))]
-    error = False
-
     user_id = -1
+
     for user_file in user_files:
-        if request.form["username"] == user_file[8:]:
-            user_id = int(user_file[:8])
+        with open(user_dir + user_file, 'r') as f:
+            data = json.load(f)
+
+            if request.form["username"] == data["username"]:
+                user_id = int(user_file[:8])
+
+        if user_id != -1: 
             break
-    if user_id = -1:
-        return "Error: User name does not exist"
-    infomation_file = user_dir + "{0:08d}".format(user_id) + ".txt"
-    with open(new_file_path, 'r') as read_file:
-        user_data = json.load(read_file)
-    if user_data["password"] != request.form["password"]:
-        return "Error: User name or password is incorrect"
-    else:
-        return "Success! You have logged in!"
     
+    if user_id == -1:
+        return "Error: User name does not exist"
+    
+    information_file = user_dir + "{0:08d}".format(user_id) + ".txt"
+    
+    with open(information_file, 'r') as read_file:
+        user_data = json.load(read_file)
+        
+        if user_data["password"] != request.form["password"]:
+            return "Error: User name or password is incorrect"
+        else:
+            session["logged_in"] = True 
+            session["user_id"] = user_id 
+            return "Success! You have logged in!"
+
+@app.route('/logout')
+def logout(): 
+    session.pop('logged_in', None)
+    session.pop('user_id', None)
+    return redirect(url_for('root'))
+
+app.secret_key = b"\x9d\x00\xb1\x80b\xd6\t'\x853V\xbf\xac2\xcd\x98Y\x88\xf3BKO\x9c\xa3"
+
+if __name__ == '__main__':
+    app.debug = True 
+    app.run()
