5d655e89ec4aa62423e7b63feef6ec47c3ce4830
ujpandey <ujpandey1@cougars.ccis.edu>
2016-04-09 23:11:02 -0500

    Add data directories if they don't exist.

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 #!venv/bin/python
 
+import os
+
 from flask import url_for
 
 from app import app
 
 if __name__ == "__main__":
+    DATA_MODELS = ['question', 'solution', 'user', 'submission', 'result']
+    for model in DATA_MODELS:
+        directory_path = os.path.join(app.config['DATA_PATH'], model)
+        if not os.path.isdir(directory_path):
+            try:
+                os.makedirs(directory_path, exist_ok=False)
+            except OSError:
+                print("Directory already exists!")
+
     app.run(debug=True, threaded=True)
